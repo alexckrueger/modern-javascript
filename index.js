665beefde3ae9da77ba9aa63f43e32788cb3405d
Alex Krueger <alexckrueger@gmail.com>
2022-02-07 13:02:05 -0600

    adds practice with dayjs and chance libraries using webpack

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+/* global dayjs */
+var dayjs = require("dayjs");
+
 console.log("oi oi oi");
+var time = dayjs("2019-01-24").daysInMonth();
+console.log(time);
+
+var Chance = require("chance");
+var chance = new Chance();
+var myRandomString = chance.string();
+
+console.log(myRandomString);
+console.log(chance.integer({ min: 0, max: 200 }));
+console.log(chance.falsy());
